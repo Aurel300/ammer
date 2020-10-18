5a9330dba6cc8dd119efe2be868f996b7fe5e014
Aurel Bílý <aurel.bily@gmail.com>
2020-10-18 12:52:34 +0200

    better null handling on hl

--- src/ammer/conv/Bytes.hl.hx
+++ src/ammer/conv/Bytes.hl.hx
@@ -4,7 +4,7 @@ import haxe.io.Bytes as HaxeBytes;
 
 abstract Bytes(HaxeBytes) from HaxeBytes to HaxeBytes {
   public static inline function fromNative(ptr:hl.Bytes, size:Int):HaxeBytes
-    return ptr.toBytes(size);
+    return ptr != null ? ptr.toBytes(size) : null;
 
   public inline function toNative1():hl.Bytes
     return @:privateAccess this.b;
--- src/ammer/conv/CString.hl.hx
+++ src/ammer/conv/CString.hl.hx
@@ -4,7 +4,7 @@ import String as HaxeString;
 
 abstract CString(HaxeString) from HaxeString to HaxeString {
   public static inline function fromNative(ptr:hl.Bytes):HaxeString
-    return @:privateAccess String.fromUTF8(ptr);
+    return ptr != null ? @:privateAccess String.fromUTF8(ptr) : null;
 
   public inline function toNative():hl.Bytes
     return this != null ? @:privateAccess this.toUtf8() : null;
