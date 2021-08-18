f42d6a9440f5ea2a70616ae66adebd12a373b810
artem pecheny <pecheny@gmail.com>
2021-08-19 00:43:41 +0300

    Fix test launcher for windows

--- tests/test-windows.bat
+++ tests/test-windows.bat
@@ -1,28 +1,39 @@
-rem @echo off
+@echo off
+
 
 echo "building native library ..."
 cd native
 nmake /f Makefile.win
 cd ..
 
+
 echo "testing hl ..."
-haxe build-hl.hxml
-if errorrlevel 0 (
-    cd bin/hl
-    copy ../../native/native.dll .
-    hl test.hl
-    cd ../..
-)
+haxe -D ammer.hl.hlInclude=%HLPATH%/include -D ammer.hl.hlLibrary=%HLPATH% build-hl.hxml
+IF %ERRORLEVEL% NEQ 0 ( echo "build of hl tests failed" && goto :cpp)
+pushd bin\hl
+copy ..\..\native\native.dll .
+hl test.hl
+popd
+IF %ERRORLEVEL% NEQ 0 ( echo "hl tests failed" && goto :cpp)
+
+
+:cpp
 
 echo "testing cpp ..."
+rd /q /s bin\cpp
+
 haxe build-cpp.hxml
-if errorrlevel 0 (
-    cd bin/cpp
-    copy ../../native/native.dll .
-    ./Main
-    cd ../..
-)
+IF %ERRORLEVEL% NEQ 0 ( echo "build of cpp tests failed" && goto :eval)
+
+cd bin\cpp
+copy ..\..\native\native.dll .
+Main.exe
+IF %ERRORLEVEL% NEQ 0 ( echo "cpp tests failed" && goto :eval)
+cd ../..
+
+:eval
 
 echo "testing eval ..."
-copy native/native.dll .
-haxe -D ammer.eval.hxDir=%TEST_HXDIR% build-cpp.hxml
+copy native\native.dll .
+haxe -D ammer.eval.haxeDir=%HAXEPATH% build-eval.hxml
+IF %ERRORLEVEL% NEQ 0 ( echo "eval tests failed")
